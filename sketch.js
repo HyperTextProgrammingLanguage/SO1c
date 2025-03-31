e9050f8fa0021c83a7f80d3a25425958bc5dbb66
HTPL <rasedo@outlook.dk>
2025-03-31 13:09:19 +0200

    variable for chaning number of cars

--- sketch.js
+++ sketch.js
@@ -1,35 +1,42 @@
-let bil1, bil2, bil3, bil4;
+let biler = [];  
+let antalBiler = 7;  
+
 function setup() {
   createCanvas(800, 400);
-  frameRate(50); strokeWeight(2);
-  bil1 = new Car("silver", createVector(75,200), createVector(1,0)); // Laver objekter af klassen "Car"
-  bil2 = new Car("gold",createVector(0,200), createVector(1,0));
-  bil3 = new Car("black",createVector(-75,200), createVector(1,0));
-  bil4 = new Car("blue",createVector(-150,200), createVector(1,0));
+  frameRate(50); 
+  strokeWeight(2);
+
+  // Opret biler baseret på antalBiler
+  for (let i = 0; i < antalBiler; i++) {
+    let farve = (i === 0) ? "silver" : (i === 1) ? "gold" : "blue";  // Forskellige farver
+    let position = createVector(-75 * i, 200);  // Placer biler med afstand
+    let hastighed = createVector(1, 0);
+    biler.push(new Car(farve, position, hastighed));  // Tilføj bil til arrayet
+  }
 }
 
 function draw() {
   background("#247B28");
     
-  bil1.update();
-  bil2.update();
-  bil3.update();
-  bil4.update();
-  bil2.tjekBilForan(bil1);
-  bil3.tjekBilForan(bil2);
-  bil4.tjekBilForan(bil3);
-  if(bil1.velocity.x < 1 )
-    bil1.velocity.x *= 1.005
+  for (let i = 0; i < biler.length; i++) {
+    biler[i].update();  // Opdater bilen
+    if (i > 0) {
+      biler[i].tjekBilForan(biler[i-1]);  // Tjek om bilen foran er for tæt på
+    }
+  }
+
+  if(biler[0].velocity.x < 1)
+    biler[0].velocity.x *= 1.005;
   
   fill(240);
   rect(-10, height/2-20, width+10, 60);
-  bil1.display();
-  bil2.display();
-  bil3.display();
-  bil4.display();
+
+  for (let i = 0; i < biler.length; i++) {
+    biler[i].display();
+  }
 }
 
 function keyPressed(){
   if(key === 'a')
-  bil1.velocity.x -= 0.3;
+    biler[0].velocity.x -= 0.3;  // Juster hastigheden på den første bil
 }
