654376aaa8145af88b9b537c2716aa34934658ee
kjullu <mikkel.aarby@outlook.dk>
2025-04-02 12:15:03 +0200

    phone test

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const phoneControl = document.querySelector(".controlButton");
-const pcControl = document.querySelector(".taster"); 
+document.addEventListener("DOMContentLoaded", () => {
+  const phoneControl = document.querySelector(".controlButton");
+  const pcControl = document.querySelector(".taster");
 
-
-// Phone check
-function isMobileDevice() {
+  // Phone check
+  function isMobileDevice() {
     return /Mobi|Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent);
   }
-  
-  if (isMobileDevice()) {
+
+  if (isMobileDevice() && phoneControl && pcControl) {
     phoneControl.style.display = 'inline-block';
     pcControl.style.display = 'none';
   }
+});
