57a7a8a26b14b64f9fc365d89c8cff0d396e9b10
kjullu <mikkel.aarby@outlook.dk>
2025-04-02 12:55:50 +0200

    gave up on reaktive height

--- sketch.js
+++ sketch.js
@@ -7,7 +7,6 @@ let lastFPS = 0; // Variable to store the last calculated FPS
 let bilAfstand = -100;
 let maxSpeed = 1;
 let width = screen.width;
-let height = screen.height;
 let startSted = width/4;
 let bilOversigt = false;
 
@@ -22,7 +21,7 @@ if (userInput === null || isNaN(userInput) || userInput === "") {
 }
 
 function setup() {
-  const myCanvas = createCanvas(width, height/4);
+  const myCanvas = createCanvas(width, 180);
   myCanvas.parent("canvas");
   frameRate(60); 
   strokeWeight(2); 
@@ -30,7 +29,7 @@ function setup() {
   // Create cars based on antalBiler variable
   for (let i = 0; i < antalBiler; i++) {
     let farve = (i === 0) ? "silver" : (i === 1) ? "gold" : "blue";  // Forskellige farver
-    let position = createVector(bilAfstand * i, (height/4)-120);  // Placer biler med afstand
+    let position = createVector(bilAfstand * i, 60);  // Placer biler med afstand
     let hastighed = createVector(maxSpeed, 0);
     biler.push(new Car(farve, position, hastighed));  // Tilføj bil til arrayet
   }
@@ -51,7 +50,7 @@ function draw() { // This function runs constantly
   }
   
   fill(240);
-  rect(-10, height/4-110, width+10, 60); // Draw road
+  rect(-10, 70, width+10, 60); // Draw road
 
   for (let i = 0; i < biler.length; i++) {
     biler[i].display();
