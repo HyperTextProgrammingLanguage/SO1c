af56133a6abddf89dd67558e5730db61fb12a1ba
kjullu <mikkel.aarby@outlook.dk>
2025-04-02 13:17:08 +0200

    made the bugfix from last commit a little better and les noticible

--- sketch.js
+++ sketch.js
@@ -132,7 +132,11 @@ function draw() { // This function runs constantly
       setTimeout(() => { // Stopper bilerne for at holde deres fart efter reset
         biler[i].position.x = bilAfstand * i; // Reset each car's position
         biler[i].velocity.x = maxSpeed; 
-      }, 300); // Run after 0.3 seconds
+      }, 30); // Run after 0.3 seconds
+      setTimeout(() => {  // Stopper bilerne for at holde deres fart efter reset
+        biler[i].position.x = bilAfstand * i; // Reset each car's position
+        biler[i].velocity.x = maxSpeed;
+      }, 60); // Run after 0.3 seconds
     }
   }
   højre.onclick = function() {
@@ -198,7 +202,11 @@ function keyPressed(){
       setTimeout(() => {  // Stopper bilerne for at holde deres fart efter reset
         biler[i].position.x = bilAfstand * i; // Reset each car's position
         biler[i].velocity.x = maxSpeed;
-      }, 300); // Run after 0.3 seconds
+      }, 30); // Run after 0.3 seconds
+      setTimeout(() => {  // Stopper bilerne for at holde deres fart efter reset
+        biler[i].position.x = bilAfstand * i; // Reset each car's position
+        biler[i].velocity.x = maxSpeed;
+      }, 60); // Run after 0.3 seconds
     }
   }
   if (key === 'e') { //animation af kø
